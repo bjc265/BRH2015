1e285f0f63499cd630a52947e3426d21b068bda8
bjc265 <bjc265@cornell.edu>
2015-09-19 02:49:25 -0400

    Add functionality for server to query any ticker for any year

--- server.js
+++ server.js
@@ -1,38 +1,58 @@
-var finalhandler = require('finalhandler');
-var http         = require('http');
-var Router       = require('router');
-var stat 		 = require('node-static');
-var AWS			 = require('aws-sdk');
+var finalhandler = require('finalhandler'),
+	http         = require('http'),
+	Router       = require('router'),
+	stat 		 = require('node-static'),
+	AWS			 = require('aws-sdk');
 
+AWS.config.update({region: 'us-east-1'});
 var fileServer = new stat.Server(); 
 var router = new Router();
 
-//loads access keys from config.json
-AWS.config.loadFromPath('./config.json');
 
-var dynamodb = new AWS.DynamoDB();
+var dynamodb = new AWS.DynamoDB.DocumentClient();
+
+var year = '2012';
+
+
+function paramsFor(ticker, year){
+	var params = {
+		TableName : year,
+		KeyConditionExpression : '#t=:ticker',
+		ExpressionAttributeNames : {
+			'#t' : 'Ticker'
+		},
+		ExpressionAttributeValues : {
+			':ticker' : ticker
+		}
+	};
+	return params;
+}
+
 
 router.get('/', function (req, res) {
   fileServer.serve(req,res);
 });
  
 
-router.get('/new/', function(req, res){
-	graphHandle(req, res);
-});
-
-
-function graphHandle(req, res){
+router.get('/new/', function(req, res) {
 	var query = req._parsedUrl.query;
-	console.log(query.substring(0,3));
-	if(!(query.substring(0,3))==="id="){
-		console.log('Received bad query: "' + query + '", ignoring.');
-		return;
-	} else{
-		console.log('Received valid request for new graph that is not: ' + query.substring(3));
-	}
-}
-
+	if(query != null){
+		if((query.substring(0,3))==="id="){
+			console.log('Received valid request for new graph that is not: ' + query.substring(3));
+		} else
+			console.log('Received bad query: "' + query + '", ignoring.'); 
+	} else
+		console.log('Received request with no query');
+	dynamodb.query(paramsFor('ACRDBT Index','2005'),function(err, data){
+		console.log("Attempted database query");
+		if(err) console.log(err, err.stack);
+		else {
+			console.log(data);
+			res.writeHead(200, {"Content-Type": "text/plain"});
+			res.end(JSON.stringify(data));
+		}
+	});
+});
 
 
 var server = http.createServer(function(req, res) {
