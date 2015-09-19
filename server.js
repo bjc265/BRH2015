489e2c228ae3761387f7daac90b68fb42568a73b
bjc265 <bjc265@cornell.edu>
2015-09-18 21:59:00 -0400

    Add AWS setup

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 var finalhandler = require('finalhandler');
 var http         = require('http');
 var Router       = require('router');
-var stat = require('node-static');
+var stat 		 = require('node-static');
+var AWS			 = require('aws-sdk');
 
 var fileServer = new stat.Server(); 
-var router = Router();
+var router = new Router();
+
+//loads access keys from config.json
+AWS.config.loadFromPath('./config.json');
+
+var dynamodb = new AWS.DynamoDB();
+
 router.get('/', function (req, res) {
   fileServer.serve(req,res);
 })
  
+
+
+
+
+
 var server = http.createServer(function(req, res) {
   router(req, res, finalhandler(req, res));
 })
